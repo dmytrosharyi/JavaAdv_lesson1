d69522c9be76b245c2e7b83224c91ae93e3c5f4f
dmytrosharyi <63544326+dmytrosharyi@users.noreply.github.com>
2020-07-21 20:56:40 +0400

    Add files via upload

---
+++ lesson1_1.js
@@ -0,0 +1,55 @@
+const People = (function () {
+    const db = ['Dmytro', 'Nataliia', 'Peter', 'Olga'];
+    let data;
+    let button;
+    let ul;
+    let cross;
+
+    function init() {
+        casheDom();
+        bindEvent();
+    }
+
+    function caсheDom() {
+        data = $('#userName');
+        button = $('#addName');
+        ul = $('#people');
+    }
+
+    function bindEvent() {
+        button.click(function () {
+            addPersonName();
+        });
+        ul.delegate('span', 'click', function (event) {
+            delPersonName(event);
+        })
+    }
+
+    function render() {
+        console.log(db);
+        ul.html('Data Base');
+        for (let i = 0; i < db.length; i++) {
+            ul.append(`<li>${db[i]}<span></span></li>`);
+        }
+    }
+
+    function addPersonName() {
+        let name = data.val();
+        db.push(name);
+        render();
+        data.val('');
+    }
+
+    function delPersonName(event) {
+        let deleted = $(event.target).closest('li');
+        let i = ul.find('li').index(deleted);
+        db.splice(i, 1);
+        render();
+    }
+
+    return {
+        init: init
+    }
+})();
+
+People.init();
